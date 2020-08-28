235da859dc57adaebbf9136444a839f185d04828
chatapazar <31641737+chatapazar@users.noreply.github.com>
2020-08-28 09:56:47 +0700

    Update hpa.sh

--- bin/hpa.sh
+++ bin/hpa.sh
@@ -1,2 +1,23 @@
 #!/bin/sh
 oc autoscale dc/dotnetapp --min 1 --max 10 --cpu-percent=50
+
+
+apiVersion: autoscaling/v2beta2 
+kind: HorizontalPodAutoscaler
+metadata:
+  name: cpu-autoscale 
+  namespace: demo-dev31
+spec:
+  scaleTargetRef:
+    apiVersion: v1 
+    kind: DeploymentConfig 
+    name: dotnetapp 
+  minReplicas: 1 
+  maxReplicas: 10 
+  metrics: 
+  - type: Resource
+    resource:
+      name: cpu 
+      target:
+        type: Utilization 
+        averageValue: 500m 
