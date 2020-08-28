ce389009587c3392a39e67450ac8b13b470f0367
chatapazar <31641737+chatapazar@users.noreply.github.com>
2020-08-28 11:00:37 +0700

    Update CustomersController.cs

--- app/Controllers/CustomersController.cs
+++ app/Controllers/CustomersController.cs
@@ -16,11 +16,11 @@ namespace Test.Controllers
         public ActionResult<IEnumerable<Customer>> Get()
         {
             //return new string[] { "chatchai", "surachai", "kittichade" };
-            
+            string appversion = Environment.GetEnvironmentVariable("app.version");
             Customer[] C = new Customer[3];
-            C[0] = new Customer { CustName  = "chatchai kongmanee", CustNo = "2020080001", PolicyNo = "20200800001"};
-			C[1] = new Customer { CustName  = "surachai kongmanee", CustNo = "2020080002", PolicyNo = "20200800002"};
-            C[2] = new Customer { CustName  = "sirikwan kongmanee", CustNo = "2020080003", PolicyNo = "20200800003"};
+            C[0] = new Customer { CustName  = "chatchai kongmanee:"+appversion, CustNo = "2020080001", PolicyNo = "20200800001"};
+			C[1] = new Customer { CustName  = "surachai kongmanee:"+appversion, CustNo = "2020080002", PolicyNo = "20200800002"};
+            C[2] = new Customer { CustName  = "sirikwan kongmanee:"+appversion, CustNo = "2020080003", PolicyNo = "20200800003"};
             
 			return C;
         }
