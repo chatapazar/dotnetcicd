444bc2fddf924746979e1c690d10983946afbfdf
chatapazar <31641737+chatapazar@users.noreply.github.com>
2020-09-03 20:34:34 +0700

    Create setup_jenkins_45.sh

---
+++ bin/setup_jenkins_45.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+oc project ci-cd
+oc new-app jenkins-persistent --param ENABLE_OAUTH=true --param MEMORY_LIMIT=4Gi --param VOLUME_CAPACITY=4Gi --param DISABLE_ADMINISTRATIVE_MONITORS=true
+oc set resources dc jenkins --limits=memory=2Gi,cpu=2 --requests=memory=1Gi,cpu=1
+oc label dc jenkins app.kubernetes.io/name=Jenkins -n ci-cd
